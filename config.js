27fc709facfd4372e02757ceb0426ba0d7fc7ab4
ngocchi111 <68102728+ngocchi111@users.noreply.github.com>
2021-11-23 19:45:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    //host: env.DB_HOST || 'localhost',
+    host: env.DB_HOST || 'localhost',
     port: env.DB_PORT || '5432',
     user: env.DB_USER || 'postgres',
     password: env.DB_PASSWORD || 'ngocchi123',
